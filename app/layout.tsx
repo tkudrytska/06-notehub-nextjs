460c1aa6fe6545f8d4762e1b9c27b0ecf38f544b
Tetiana Kudrytska <tdolinkina@gmail.com>
2026-05-27 00:34:18 +0200

    add home page

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "./components/Header/Header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "NoteHub",
+  description: "A simple note-taking app built with Next.js and Tailwind CSS.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <Header />
+        <main>{children}</main>
+        <footer></footer>
+      </body>
+    </html>
+  );
+}
